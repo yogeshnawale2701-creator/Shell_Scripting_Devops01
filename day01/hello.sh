54dec01f82c8874dd71fd0ca510df583bb4b5cb2
Ubuntu <ubuntu@ip-172-31-35-79.eu-north-1.compute.internal>
2025-09-06 10:59:48 +0000

    Add Day01 folder with shell scripting basics

---
+++ day01/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+# this is a script fot learning
+
+
+echo "YVN: This is Yogesh Nawale"
+
+echo "YVN: I am a learner"
+
+echo "YVN: I will become the best or the new big thing in linux or devops"
